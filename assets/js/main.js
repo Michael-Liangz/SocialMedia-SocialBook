1d346573e818feb9871156030d765a64ce56236f
Michael-Liangz <Michaelliangz.ml@gmail.com>
2023-01-19 21:01:59 +0700

    updated

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+//  TODO =============== PROFILE-SETTING-MENU ===============
+var settingsMenu = document.querySelector(".settings-menu");
+
+function settingsMenuToggle() {
+  settingsMenu.classList.toggle("settings-menu-height");
+}
