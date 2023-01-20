9b9b6fb39668fa37a2b1a26beb46a69de5be7e9a
Michael-Liangz <Michaelliangz.ml@gmail.com>
2023-01-20 17:19:03 +0700

    updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,3 +4,27 @@ var settingsMenu = document.querySelector(".settings-menu");
 function settingsMenuToggle() {
   settingsMenu.classList.toggle("settings-menu-height");
 }
+
+// TODO =============== LIGHT / DARK THEME BUTTON ===============
+var darkBtn = document.getElementById("dark-btn");
+
+darkBtn.onclick = function () {
+  darkBtn.classList.toggle("dark-btn-on");
+  document.body.classList.toggle("dark-theme");
+
+  if (localStorage.getItem("theme") == "light") {
+    localStorage.setItem("theme", "dark");
+  } else {
+    localStorage.setItem("theme", "light");
+  }
+};
+
+if (localStorage.getItem("theme") == "light") {
+  darkBtn.classList.remove("dark-btn-on");
+  document.body.classList.remove("dark-theme");
+} else if (localStorage.getItem("theme") == "dark") {
+  darkBtn.classList.add("dark-btn-on");
+  document.body.classList.add("dark-theme");
+} else {
+  localStorage.setItem("theme", "light");
+}
